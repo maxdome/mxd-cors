b2f9f79c71172ad56c1c8f0d774adea8ff55ac97
Fabian Schneider <fabian.schneider@maxdome.de>
2017-02-17 14:24:00 +0100

    Fix device whitelisting

--- src/lib.js
+++ src/lib.js
@@ -18,6 +18,7 @@ module.exports = (opts) => {
           allowedHeaders: ['accept', 'content-type', 'platform', 'clienttype', 'x-device'],
           maxAge: 3600,
           optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+          preflightContinue: true
         },
         opts,
         (objValue, srcValue) => {
@@ -28,17 +29,24 @@ module.exports = (opts) => {
       )
     )(req, res, (err) => {
       if (err) {
-        next(err);
-      } else {
-        const devices = ['Media/BP440', 'Media/BP530', 'Media/BP540', 'Media/BH6430', 'Media/BP550', 'Media/BP730'];
-        if (req.headers['platform'] === 'ott' && req.headers['clienttype'] === 'lg_bd' && devices.indexOf(req.headers['x-device']) > -1) {
-          res.setHeader('access-control-allow-origin', '*');
-          res.removeHeader('access-control-allow-credentials');
-        } else if (res.get('access-control-allow-origin') === undefined && res.get('access-control-allow-credentials') !== undefined) {
-          res.removeHeader('access-control-allow-credentials');
-        }
-        next();
+        return next(err);
       }
+
+      const method = req.method && req.method.toUpperCase && req.method.toUpperCase();
+      const devices = ['Media/BP440', 'Media/BP530', 'Media/BP540', 'Media/BH6430', 'Media/BP550', 'Media/BP730'];
+
+      if (req.headers['platform'] === 'ott' && req.headers['clienttype'] === 'lg_bd' && devices.indexOf(req.headers['x-device']) > -1) {
+        res.setHeader('access-control-allow-origin', '*');
+        res.removeHeader('access-control-allow-credentials');
+      } else if (res.get('access-control-allow-origin') === undefined && res.get('access-control-allow-credentials') !== undefined) {
+        res.removeHeader('access-control-allow-credentials');
+      }
+
+      if (method === 'OPTIONS') {
+        return res.status(200).end();
+      }
+
+      return next();
     });
   };
 };
